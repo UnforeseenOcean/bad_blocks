d343f09396a5d83419213e33e27389f01bc3e2fe
mor-rask <gingont@gmail.com>
2016-03-24 16:48:30 -0300

    Update skype.py

--- skype.py
+++ skype.py
@@ -17,12 +17,12 @@ distr = random.choice(['cs', 'rust', 'pb', 'minecraft','lol', 'wow', 'crossfire'
 
 if lan == 'pt_BR':
     message0 = random.choice(['Olha so o que eu achei kkkk| ', 'Voce nao vai acreditar nisso| ', 'Viu o novo lancamento?| '])
-    message1 = message0 + 'Hacks de' + distr + ": YOUR URL GOES HERE"
+    message1 = message0 + 'Hacks de' + distr + ": YOUR URL GOES IN HERE"
     ctypes.windll.user32.MessageBoxA(0, "Nova atualizacao Skype! Aceite o plugin para baixa-la!", "Skype", 1)
 
 else:
     message0 = random.choice(['Take a look at what I have just found! LOLOLOL| ', 'Unbelievable!!| ', 'Have you seen the news?| '])
-    message1 = message0 + distr + " cheats: YOUR URL GOES HERE"
+    message1 = message0 + distr + " cheats: YOUR URL GOES IN HERE"
     ctypes.windll.user32.MessageBoxA(0, "New Skype update! Accept the plugin to download it!", "Skype", 1)
 
 skype = Skype4Py.Skype()
